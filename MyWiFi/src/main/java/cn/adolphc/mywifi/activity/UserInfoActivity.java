2802045de7e15775c98c626b6c54ff872b3f0fce
adolph58 <jaunty5@sina.com>
2017-02-20 22:14:08 +0800

    增加个人信息界面

---
+++ MyWiFi/src/main/java/cn/adolphc/mywifi/activity/UserInfoActivity.java
@@ -0,0 +1,25 @@
+package cn.adolphc.mywifi.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.adolphc.mywifi.R;
+
+public class UserInfoActivity extends BaseActivity {
+
+    @Override
+    protected void initVariables() {
+
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_user_info);
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+}
